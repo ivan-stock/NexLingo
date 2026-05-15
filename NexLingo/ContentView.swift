8922aae4458f7be1cb2b5dd74f109cfa44d485b5
TheSilentMadman <clotmine@gmail.com>
2026-05-15 14:14:22 -0400

    Initial Commit

---
+++ NexLingo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NexLingo
+//
+//  Created by 胖鱼头 on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NexLingo/NexLingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexLingoApp.swift
+//  NexLingo
+//
+//  Created by 胖鱼头 on 2026/5/15.
+//
+
+import SwiftUI
+
+@main
+struct NexLingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
