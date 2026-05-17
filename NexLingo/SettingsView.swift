fcaf2a6e53441b883da99085d5e0505412d6d9a2
TheSilentMadman <clotmine@gmail.com>
2026-05-17 11:10:48 -0400

    Add privacy policy web page

---
+++ NexLingo/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  NexLingo
+//
+//  Created by 胖鱼头 on 2026/5/15.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
